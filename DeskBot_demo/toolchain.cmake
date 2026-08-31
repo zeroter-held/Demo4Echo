6bf4d2da90aa12d64d080a4b929f3c4c105a0b09
零末 <2280656215@qq.com>
2026-08-31 13:22:31 +0800

    build: 支持 RV1106_SDK_PATH 环境变量配置，导出 LVGL 子模块补丁

--- DeskBot_demo/toolchain.cmake
+++ DeskBot_demo/toolchain.cmake
@@ -2,14 +2,19 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(SDK_PATH "/home/kingham/Projects/git_projects/Echo-Mate/SDK/rv1106-sdk")
+if(DEFINED ENV{RV1106_SDK_PATH})
+    file(TO_CMAKE_PATH "$ENV{RV1106_SDK_PATH}" SDK_PATH)
+else()
+    set(SDK_PATH "/home/zxx360zxx/Projects/Echo-Mate/SDK/rv1106-sdk")
+endif()
+set(TOOLCHAIN_PATH ${SDK_PATH}/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf)
 
 # Specify the compiler paths
-set(CMAKE_C_COMPILER ${SDK_PATH}/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-gcc)
-set(CMAKE_CXX_COMPILER ${SDK_PATH}/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-g++)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PATH}/bin/arm-rockchip830-linux-uclibcgnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PATH}/bin/arm-rockchip830-linux-uclibcgnueabihf-g++)
 
 # Specify the sysroot (if available)
-set(CMAKE_SYSROOT ${SDK_PATH}/sysdrv/source/buildroot/buildroot-2023.02.6/output/host/arm-buildroot-linux-uclibcgnueabihf/sysroot)
+set(CMAKE_SYSROOT ${TOOLCHAIN_PATH}/arm-rockchip830-linux-uclibcgnueabihf/sysroot)
 
 # Add paths to find libraries and includes
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
